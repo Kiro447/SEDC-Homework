3e3edf3c7391e7fddc6f6703a90218988f6fe78a
Kiril Jordanov <jordanov.k@outlook.com>
2022-12-10 02:05:09 +0100

    check

--- Homework-Three/Homework-Bonus/script.js
+++ Homework-Three/Homework-Bonus/script.js
@@ -1,16 +1,16 @@
-function shouldServeDrinks (){
+function shouldServeDrinks() {
     let personAge = parseInt(prompt('Enter your age'));
-    let bartenderBreak = Math.random()<0.5 ;
+    let bartenderBreak = Math.random() < 0.5;
     console.log(bartenderBreak);
-    if ( personAge >= 18 && bartenderBreak === true){
+    if (personAge >= 18 && bartenderBreak === true) {
         alert('The bartender is on break');
         shouldServeDrinks();
-    } else if ( personAge >= 18 && bartenderBreak !== true){
+    } else if (personAge >= 18 && bartenderBreak !== true) {
         alert('You can have a whisky on rocks')
-        shouldServeDrinks(); 
+        shouldServeDrinks();
     } else {
         alert('Youre too young')
-        shouldServeDrinks(); 
+        shouldServeDrinks();
     }
 
     // switch (personAge,bartenderBreak){
@@ -29,6 +29,6 @@ function shouldServeDrinks (){
     //         shouldServeDrinks();
     //         break;
     // }
-    
+
 }
 shouldServeDrinks();
