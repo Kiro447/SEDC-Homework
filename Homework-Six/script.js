e89d3b68960544c48a2ae4f791eed8927a2c2bf9
Kiril Jordanov <jordanov.k@outlook.com>
2022-12-23 22:24:59 +0100

    sixth

--- Homework-Six/script.js
+++ Homework-Six/script.js
@@ -1,10 +1,28 @@
-let table = document.getElementById("myTable");
-let form = document.querySelector('form');
-let 
+let mainDiv = document.getElementById('main');
+let inputRow = document.getElementById('rows');
+let inputColumn = document.getElementById('column')
+let buttonGen = document.getElementById('btn');
 
-function addRow(rows){
+let table = document.createElement('table');
+let tbody = document.createElement('tbody');
+table.style = "border: 1px solid black";
+table.appendChild(tbody);
+mainDiv.appendChild(table);
 
-}
-function addColumn(){
-
-}
+buttonGen.addEventListener('click', ()=>{
+    let row = inputRow.value;
+    let column = inputColumn.value;
+    for (let i = 0 ; i < row; i++){
+        let rowCreated = document.createElement('tr');
+        rowCreated.style = "border: 1px solid black; height: 3rem;";;
+        console.log(row);
+        for (let j = 0 ; j < column ; j++){
+            console.log(j);
+            let columnCreated = document.createElement('td');
+            columnCreated.style = "border: 1px solid black; font-size: 1em; width: 10rem"
+            columnCreated.innerText = `Row-${i+1} column-${j+1}`;
+            rowCreated.appendChild(columnCreated);
+        }
+        tbody.appendChild(rowCreated)
+    }
+})
